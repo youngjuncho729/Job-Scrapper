160e4954f1ce6f28b9065d502fa91595d6c741dc
Youngjun Cho <55816062+youngjuncho729@users.noreply.github.com>
2022-02-13 17:29:56 +0000

    Save output into csv file

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from indeed import get_indeed
 from stackoverflow import get_stackoverflow
+from export import save_to_csv
 
+# Extract jobs from indeed.com
 indeed_jobs = get_indeed()
+# Extract jobs from stackoverflow.com
 stackoverflow_jobs = get_stackoverflow()
+
 jobs = indeed_jobs + stackoverflow_jobs
-print(jobs)
+
+# Save results into csv file
+save_to_csv(jobs)
